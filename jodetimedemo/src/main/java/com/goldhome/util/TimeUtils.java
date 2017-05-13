d2f339e2712a6b3d36dcc40313046200026879ab
yanruilin <yanruilin@139.com>
2017-05-13 14:08:23 +0800

    jodatime 时间共同类，解决夏令时问题

---
+++ jodetimedemo/src/main/java/com/goldhome/util/TimeUtils.java
@@ -0,0 +1,143 @@
+package com.goldhome.util;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+/**
+ * 时间处理共同类
+ * Created by yanruilin on 2017/5/13.
+ */
+public class TimeUtils
+{
+    /**
+     * 设置系统时区
+     * @param zone 时区
+     */
+    public static void setSystemTimeZone(String zone)
+    {
+        //设置系统时区
+        DateTimeZone.setDefault(DateTimeZone.forID(zone));
+    }
+
+    /**
+     * 判断该UTC时间是否是夏令时
+     * @param utc UTC时间
+     * @return
+     */
+    public static boolean isDaylightTime(long utc)
+    {
+        DateTimeZone zone = DateTimeZone.getDefault();
+        boolean isStandardOffset = zone.isStandardOffset(utc * 1000);
+        return !isStandardOffset;
+    }
+
+    /**
+     * 日期转换为UTC时间
+     * @param date 日期
+     * @return utc时间
+     */
+    public static long dateToUtc(String date, String pattern)
+    {
+        long utc;
+
+        DateTimeFormatter format = DateTimeFormat.forPattern(pattern);
+        DateTime  dateTime = DateTime.parse(date, format);
+
+        utc = dateTime.getMillis() / 1000;
+        return utc;
+    }
+
+    /**
+     * UTC时间转换为日期
+     * @param utc UTC 时间
+     * @param pattern 格式化
+     * @return 日期
+     */
+    public static String utcToDate(long utc, String pattern)
+    {
+        DateTime dateTime = new DateTime(utc * 1000);
+        return dateTime.toString(pattern);
+    }
+
+
+    /**
+     * 获取当前时间
+     * @param pattern 格式化
+     * @return 当前日期
+     */
+    public static String getCurrentDate(String pattern)
+    {
+        return Instant.now().toDateTime().toString(pattern);
+    }
+
+    /**
+     * 获取当前时区偏移量
+     * @param utc UTC时间
+     * @return
+     */
+    public static int getTimeZoneOffset(long utc)
+    {
+        DateTimeZone zone = DateTimeZone.getDefault();
+
+        int offset = zone.getOffset(utc * 1000);
+        return offset / 1000;
+    }
+
+    /**
+     * 如果是夏令时时间，添加上(DST)
+     * @param dateStr 日期
+     * @param pattern 格式化
+     * @return 返回修改后的日期
+     */
+    public static String dateAddDst(String dateStr, String pattern)
+    {
+        long utc = dateToUtc(dateStr, pattern);
+        if (isDaylightTime(utc))
+        {
+            dateStr += "(DST)";
+        }
+
+        return dateStr;
+    }
+
+    /**
+     * 获取天表后缀
+     * 当前时间距离19700101有多少天
+     * @param utc
+     * @return
+     */
+    public static int getDaySuff(long utc)
+    {
+       DateTime dateTime = new DateTime(utc * 1000);
+
+       int hour = dateTime.getHourOfDay();
+       int diff = 12 - hour;
+
+       if (diff > 0)
+       {
+           dateTime = dateTime.plusHours(diff);
+       }
+       else
+       {
+           dateTime = dateTime.minusHours(Math.abs(diff));
+       }
+
+       String result = String.valueOf(dateTime.getMillis() / new Long(24 * 60 * 60 * 1000));
+
+        return Integer.parseInt(result);
+    }
+
+    /**
+     * 获取月表后缀
+     * 当前时间距离19700101有多少个月
+     * @param utc
+     * @return
+     */
+    public static int getMonthSuff(long utc)
+    {
+        return 0;
+    }
+}
---
+++ jodetimedemo/src/test/java/com/goldhome/util/TimeUtilsTest.java
@@ -0,0 +1,90 @@
+package com.goldhome.util;
+
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Administrator on 2017/5/13.
+ */
+public class TimeUtilsTest
+{
+    private static Logger logger = LoggerFactory.getLogger(TimeUtilsTest.class);
+
+    @BeforeClass
+    public static void globalInit()
+    {
+    }
+
+    @AfterClass
+    public static void globalDestory()
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        TimeUtils.setSystemTimeZone("America/Santiago");
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void dateToUtcTest()
+    {
+        String format = "yyyy-MM-dd HH:mm";
+        long utc = TimeUtils.dateToUtc("2017-05-13 23:00", format);
+        System.out.println(String.valueOf(utc));
+        //2017-05-13 23:00(DST) ---- 1494727200
+        //2017-05-14 00:00 ---- 1494734400
+    }
+
+    @Test
+    public void utcToDateTest()
+    {
+        String format = "yyyy-MM-dd HH:mm";
+        long utc = TimeUtils.dateToUtc("2017-05-13 23:00", format);
+        String dateStr = TimeUtils.utcToDate(utc, format);
+        System.out.println(dateStr);
+    }
+
+    @Test
+    public void isDaylightTimeTest()
+    {
+        System.out.println("2017-05-13 23:00(DST) is lightDay ?" + TimeUtils.isDaylightTime(1494727200));
+        System.out.println("2017-05-14 00:00 is lightDay ?" + TimeUtils.isDaylightTime(1494734400));
+    }
+
+    @Test
+    public void getCurrentDateTest()
+    {
+        String format = "yyyy-MM-dd HH:mm";
+        System.out.println(TimeUtils.getCurrentDate(format));
+    }
+
+    @Test
+    public void getTimeZoneOffsetTest()
+    {
+        System.out.println(TimeUtils.getTimeZoneOffset(1494727200L));
+        System.out.println(TimeUtils.getTimeZoneOffset(1494734400L));
+    }
+
+    @Test
+    public void dateAddDstTest()
+    {
+        String format = "yyyy-MM-dd HH:mm";
+        System.out.println(TimeUtils.dateAddDst("2017-05-13 23:00", format));
+        System.out.println(TimeUtils.dateAddDst("2017-05-14 00:00", format));
+    }
+
+    @Test
+    public void getDaySuffTest()
+    {
+        System.out.println(TimeUtils.getDaySuff(1494727200L));
+        System.out.println(TimeUtils.getDaySuff(1494734400L));
+    }
+
+}
